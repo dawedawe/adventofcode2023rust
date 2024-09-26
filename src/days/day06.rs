bb0f38b712e23bd38b3e2143c1e2af022fdf426c
dawe <dawedawe@posteo.de>
2024-09-26 17:15:53 +0200

    day 06 part 2

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -2,16 +2,21 @@ use std::fs;
 
 const INPUT: &str = "day06input.txt";
 
-fn parse_numbers(line: &str) -> Vec<i32> {
+fn parse_numbers(line: &str) -> Vec<i64> {
     line.split_whitespace()
         .skip(1)
         .map(|n| n.parse().unwrap())
-        .collect::<Vec<i32>>()
+        .collect::<Vec<i64>>()
 }
 
-fn calc_winning_ways((time, record): (i32, i32)) -> i32 {
+fn parse_numbers_part2(line: &str) -> i64 {
+    let number: i64 = line.split_whitespace().skip(1).collect::<String>().parse().unwrap();
+    number
+}
+
+fn calc_winning_ways((time, record): (i64, i64)) -> i64 {
     let r = 1..time;
-    r.map(|t| t * (time - t) > record).filter(|x| *x).count() as i32
+    r.map(|t| t * (time - t) > record).filter(|x| *x).count() as i64
 }
 
 pub fn day06_part1() {
@@ -20,6 +25,15 @@ pub fn day06_part1() {
     let times = parse_numbers(lines[0]);
     let records = parse_numbers(lines[1]);
     let races = times.into_iter().zip(records);
-    let prod: i32 = races.map(calc_winning_ways).product();
+    let prod: i64 = races.map(calc_winning_ways).product();
     println!("{}", prod);
+}
+
+pub fn day06_part2() {
+    let input = fs::read_to_string(INPUT).expect("read_to_string failed");
+    let lines: Vec<&str> = input.lines().collect();
+    let time = parse_numbers_part2(lines[0]);
+    let record = parse_numbers_part2(lines[1]);
+    let winning_ways = calc_winning_ways((time, record));
+    println!("{}", winning_ways);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use adventofcode2023rust::days;
 
 fn main() {
-    days::day06::day06_part1();
+    days::day06::day06_part2();
 }
