c111392083109b72982c00a09b46b09074d2e661
dawe <dawedawe@posteo.de>
2024-09-28 16:50:39 +0200

    day 08 part 2

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -59,3 +59,47 @@ pub fn part1() {
 
     println!("{steps}")
 }
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+pub fn part2() {
+    let input = fs::read_to_string(INPUT).expect("read_to_string failed");
+    let lines: Vec<&str> = input.lines().collect();
+    let directions = parse_directions(lines[0]);
+    let map_lines = lines.into_iter().skip(2).collect::<Vec<&str>>();
+    let map = parse_map(map_lines);
+    let current_nodes = map
+        .keys()
+        .filter(|n| n.ends_with("A"))
+        .map(|n| map.get(n).unwrap())
+        .collect::<Vec<&Node>>();
+    let mut needed_steps: Vec<usize> = vec![];
+
+    for mut current in current_nodes {
+        let mut steps = 0;
+        while !current.name.ends_with("Z") {
+            let dir = directions[steps % directions.len()];
+            let next = if dir == 'L' {
+                &current.left[..]
+            } else {
+                &current.right[..]
+            };
+            current = map.get(next).unwrap();
+            steps += 1;
+        }
+        needed_steps.push(steps);
+    }
+
+    let lcm_needed = needed_steps.iter().cloned().reduce(lcm).unwrap();
+    println!("{}", lcm_needed);
+}
